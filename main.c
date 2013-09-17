23bdd312836f4d9f83351a35643f9b655e75fa56
Wang Lifeng <fengeryes@126.com>
2013-09-17 14:39:35 +0800

    half way for merge segment

--- main.c
+++ main.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<unistd.h>
+#inlcude<string.h>
 #define MAX_SEG 10
 int main()
 {
-    FILE *fpReadTemplate,*fpReadElement,*fpWrite;
+    FILE *fpRead,*fpWrite=NULL;
     char *buff;
+    char *wbuff[80],*pwbuff;
+    char mergebuff[30];
     char *pSegment[MAX_SEG];
     int length;
-    int i,j,k,temp;
-    int flag;
-    if((fpReadTemplate=fopen("source","rb+"))==NULL)
+    int i,j,k,temp,segmentNum;
+    int flag,round;
+    if((fpRead=fopen("source","rb+"))==NULL)
     {
         printf("can not open template file\n");
         sleep(2);
         return 1;
     }
     printf("file read success\n");
-    fseek(fpReadTemplate,0,SEEK_END);
-    length=ftell(fpReadTemplate);
+    fseek(fpRead,0,SEEK_END);
+    length=ftell(fpRead);
     buff=(char *)malloc(sizeof(char)*(length+1));
-    fseek(fpReadTemplate,0,SEEK_SET);
-    fgets(buff,length+1,fpReadTemplate);
+    fseek(fpRead,0,SEEK_SET);
+    fgets(buff,length+1,fpRead);
+    fclose(fpRead);
     i=0;
     j=1;
     pSegment[i]=buff;
@@ -48,15 +52,38 @@ int main()
        }
         j++;
     }
+    segmentNum=i+1;
     /////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////
-    if((fpReadElement=fopen("element","rb+"))==NULL)
+    if((fpRead=fopen("element","rb+"))==NULL)
     {
         printf("can not open element file\n");
         sleep(2);
         return 2;
     }
-    for(j=0;j<=i;j++)
-    puts(pSegment[j]);
+    round=0;
+    mergebuff[0]=0;
+    while(fgets(wbuff,sizeof(wbuff),fpRead))
+    {
+        i=0;
+        pwbuff=wbuff;
+        while(i<sizeof(wbuff)&&wbuff[i])
+        {
+            while(wbuff[i]&&wbuff[i]!=',')
+                i++;
+            wbuff[i]=0;
+            strcat(mergebuff,pwbuff);
+            pwbuff=pwbuff+i+1;
+            if(fpWrite==NULL)
+                fpWrite=fopen(mergebuff,"wb+");
+            switch(mode)
+            {
+                case 0:break;//creat a new file;
+                case 1:break;//file
+            }
+        }
+
+    }
+
     return 0;
 }
