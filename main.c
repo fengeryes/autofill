561dc443cef1dcae8238fc567ddfcd27718e1ffa
Wang Lifeng <fengeryes@126.com>
2013-09-17 09:31:33 +0800

    add read mothod,read entire file to memory

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fp;
+    char *buff;
+    int length;
+    if((fp=fopen("source","rb+"))==NULL)
+        return 1;
+    printf("file read success\n");
+    fseek(fp,0,SEEK_END);
+    length=ftell(fp);
+    buff=(char *)malloc(sizeof(char)*(length+1));
+    fseek(fp,0,SEEK_SET);
+    fgets(buff,length+1,fp);
+    puts(buff);
+    return 0;
+}
