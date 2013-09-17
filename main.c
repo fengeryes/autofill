508033f3aeaed5b2831ee50815189be45296e059
Wang Lifeng <fengeryes@126.com>
2013-09-17 13:44:44 +0800

    add fail promopt and change some file point's name

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<unistd.h>
 #define MAX_SEG 10
 int main()
 {
-    FILE *fpRead,*fpWrite;
+    FILE *fpReadTemplate,*fpReadElement,*fpWrite;
     char *buff;
     char *pSegment[MAX_SEG];
     int length;
     int i,j,k,temp;
     int flag;
-    if((fpRead=fopen("source","rb+"))==NULL)
+    if((fpReadTemplate=fopen("source","rb+"))==NULL)
+    {
+        printf("can not open template file\n");
+        sleep(2);
         return 1;
+    }
     printf("file read success\n");
-    fseek(fpRead,0,SEEK_END);
-    length=ftell(fpRead);
+    fseek(fpReadTemplate,0,SEEK_END);
+    length=ftell(fpReadTemplate);
     buff=(char *)malloc(sizeof(char)*(length+1));
-    fseek(fpRead,0,SEEK_SET);
-    fgets(buff,length+1,fpRead);
+    fseek(fpReadTemplate,0,SEEK_SET);
+    fgets(buff,length+1,fpReadTemplate);
     i=0;
     j=1;
     pSegment[i]=buff;
@@ -42,6 +47,14 @@ int main()
            }
        }
         j++;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////
+    if((fpReadElement=fopen("element","rb+"))==NULL)
+    {
+        printf("can not open element file\n");
+        sleep(2);
+        return 2;
     }
     for(j=0;j<=i;j++)
     puts(pSegment[j]);
