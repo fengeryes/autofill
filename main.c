e1073d9b62350b1006dca5fd8d8ca0260b14cb51
Wang Lifeng <fengeryes@126.com>
2013-09-17 13:30:03 +0800

    div the source file into pieces

--- main.c
+++ main.c
@@ -1,19 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX_SEG 10
 int main()
 {
-    FILE *fp;
+    FILE *fpRead,*fpWrite;
     char *buff;
+    char *pSegment[MAX_SEG];
     int length;
-    if((fp=fopen("source","rb+"))==NULL)
+    int i,j,k,temp;
+    int flag;
+    if((fpRead=fopen("source","rb+"))==NULL)
         return 1;
     printf("file read success\n");
-    fseek(fp,0,SEEK_END);
-    length=ftell(fp);
+    fseek(fpRead,0,SEEK_END);
+    length=ftell(fpRead);
     buff=(char *)malloc(sizeof(char)*(length+1));
-    fseek(fp,0,SEEK_SET);
-    fgets(buff,length+1,fp);
-    puts(buff);
+    fseek(fpRead,0,SEEK_SET);
+    fgets(buff,length+1,fpRead);
+    i=0;
+    j=1;
+    pSegment[i]=buff;
+    while(buff[j])
+    {
+       if(buff[j]=='#')
+       {
+           flag=1;
+           temp=j++;
+           for(k=0;(k<3)&&buff[j];k++,j++)
+           {
+               if(buff[j]!='$')
+               {
+                   flag=0;
+                   j--;
+                   break;
+               }
+           }
+           if(flag)
+           {
+               buff[temp]=0;
+               pSegment[++i]=buff+j;
+           }
+       }
+        j++;
+    }
+    for(j=0;j<=i;j++)
+    puts(pSegment[j]);
     return 0;
 }
